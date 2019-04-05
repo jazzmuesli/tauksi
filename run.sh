dcf2ca134201ff58331dc0d65f3ab7bec6352745
preich <preich@192.168.1.101>
2019-04-05 23:57:41 +0100

    non-interactive

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 docker stop tauksi-container
 docker rm tauksi-container
 docker build . -t tauksi:v1
-docker run -it --name tauksi-container tauksi:v1 /bin/bash /root/build-projects.sh
+# non-interactive
+docker run --name tauksi-container tauksi:v1 /bin/bash /root/build-projects.sh
 docker cp tauksi-container:/root/metrics.zip ./
 
+# attach if necessary: docker  exec -it tauksi-container /bin/bash
