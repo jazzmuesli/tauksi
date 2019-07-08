932416d4b88d9e39fbf5bef82d61bee5443f8358
preich <preich@192.168.1.101>
2019-07-09 00:30:42 +0100

    example

---
+++ plugin/src/main/java/org/pavelreich/saaremaa/ExampleClass.java
@@ -0,0 +1,19 @@
+package org.pavelreich.saaremaa;
+
+public class ExampleClass {
+
+	public static void main(String[] args) {
+		int i = 0;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		System.out.println(i);
+	}
+}
