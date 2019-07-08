e6770421ac6f24f47581b00d4d07b38ba9c5b348
preich <preich@192.168.1.101>
2019-07-09 00:31:18 +0100

    refactored example

--- plugin/src/main/java/org/pavelreich/saaremaa/ExampleClass.java
+++ plugin/src/main/java/org/pavelreich/saaremaa/ExampleClass.java
@@ -4,16 +4,21 @@ public class ExampleClass {
 
 	public static void main(String[] args) {
 		int i = 0;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
-		i += 1;
+		i = add10(i);
 		System.out.println(i);
+	}
+
+	private static int add10(int i) {
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		return i;
 	}
 }
