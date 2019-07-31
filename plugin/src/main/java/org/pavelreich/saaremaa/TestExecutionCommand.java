a37039bb5083e429474dfb63c48847b9b43d6a0b
preich <preich@192.168.1.101>
2019-08-01 00:09:03 +0100

    execute tests by method

---
+++ plugin/src/main/java/org/pavelreich/saaremaa/TestExecutionCommand.java
@@ -0,0 +1,27 @@
+package org.pavelreich.saaremaa;
+
+import org.bson.Document;
+
+public class TestExecutionCommand {
+	String testClassName;
+	String testMethodName;
+
+	public static TestExecutionCommand forTestClass(String className) {
+		TestExecutionCommand cmd = new TestExecutionCommand();
+		cmd.testClassName = className;
+		return cmd;
+	}
+	
+	public static TestExecutionCommand forTestClassMethod(String className, String methodName) {
+		TestExecutionCommand cmd = forTestClass(className);
+		cmd.testMethodName = methodName;
+		return cmd;
+	}
+
+	public Document asDocument() {
+		Document doc = new Document();
+		doc = doc.append("testClassName", testClassName).append("testMethodName", testMethodName);
+		return doc;
+	}
+
+}
