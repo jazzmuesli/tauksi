78f0e2f020af056939ad5d385d27b1157e543eb1
Pavel Reich <preich@mhetzner.openfun.org>
2019-08-14 01:59:43 +0200

    remove mojo for now

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,9 @@
 
 docker stop tauksi2-container
 docker rm tauksi2-container
-docker build . -t tauksi2:v1
+docker build . -t tauksi2:v2
 # non-interactive
-docker run -p 37017:27017 -it --name tauksi2-container tauksi2:v1 /bin/bash
+docker run -p 37017:27017 -it --name tauksi2-container tauksi2:v2 /bin/bash
 #docker cp tauksi-container:/root/metrics.zip ./
 
 # attach if necessary: docker  exec -it tauksi-container /bin/bash
