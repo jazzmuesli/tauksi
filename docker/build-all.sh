605d6d663065d791a703db4bab68aa50e923ffc0
preich <preich@192.168.1.101>
2019-08-15 01:17:31 +0100

    fixed

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -11,7 +11,9 @@ do
 	echo $i
 	cd /projects/$i
 	git clean -f -d	
-	mvn install | tee inst.txt
-	mvn org.pavelreich.saaremaa:plugin:metrics | tee metr.txt
-	mvn -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=15 org.pavelreich.saaremaa:plugin:ctest | tee ctest.txt
+	root="/projects/`pwd | tr '/' '_'`"
+	mkdir -p $root
+	mvn install | tee $root/inst.txt
+	mvn org.pavelreich.saaremaa:plugin:metrics | tee $root/metr.txt
+	mvn -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=15 org.pavelreich.saaremaa:plugin:ctest | tee $root/ctest.txt
 done
