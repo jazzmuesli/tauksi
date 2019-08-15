6753d4d1e4f0c7763e7ce739f97336e2298b75f5
preich <preich@192.168.1.101>
2019-08-15 01:11:32 +0100

    run tests before

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -5,11 +5,12 @@ do
 	cd /projects && git clone $i
 done
 
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 for i in `ls /projects`;
 do 
 	echo $i
 	cd /projects/$i
-	
+	git clean -f -d	
 	mvn install | tee inst.txt
 	mvn org.pavelreich.saaremaa:plugin:metrics | tee metr.txt
 	mvn -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=15 org.pavelreich.saaremaa:plugin:ctest | tee ctest.txt
