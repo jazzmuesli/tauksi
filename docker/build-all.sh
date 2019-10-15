5b8ef64257526f0542169073bd5ba2e9fc1f081e
preich <preich@192.168.1.101>
2019-10-15 23:28:24 +0100

    use default java

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,12 +1,15 @@
 nohup mongod --bind_ip_all &
 mkdir /projects
 mkdir /logs
-for i in `cat /root/tauksi/docker/projects.txt`;
+for i in `cat /root/tauksi/docker/projects.txt | head -1`;
 do
 	cd /projects && git clone $i
 done
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+java -version 2>&1 | tee /logs/java.version
+javac -version 2>&1 | tee /logs/javac.version
+
 for i in `ls /projects`;
 do 
 	echo $i
@@ -19,5 +22,5 @@ do
 	mvn -DmemoryInMB=9000 -Dcores=6 -DtimeInMinutesPerClass=2 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate org.evosuite.plugins:evosuite-maven-plugin:LATEST:export | tee $root/evosuite.txt
 	mvn org.pavelreich.saaremaa:plugin:metrics | tee $root/metr.txt
 	mvn -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=15 org.pavelreich.saaremaa:plugin:ctest | tee $root/ctest.txt
-	mvn -DwithHistory -DoutputFormats=CSV,XML,HTML  org.pitest:pitest-maven:mutationCoverage | tee $root/pitest.txt
+	mvn -Dverbose=true -DwithHistory -DoutputFormats=CSV,XML,HTML  org.pitest:pitest-maven:mutationCoverage | tee $root/pitest.txt
 done
