98ee933ec5a4fc22e32f04654e726b8a95daf231
preich <preich@192.168.1.101>
2019-10-15 22:04:47 +0100

    re-use ck-mvn-plugin

--- plugin/src/main/java/org/pavelreich/saaremaa/CodeMetricsMojo.java
+++ plugin/src/main/java/org/pavelreich/saaremaa/CodeMetricsMojo.java
@@ -3,7 +3,6 @@ package org.pavelreich.saaremaa;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,6 +13,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -22,7 +22,11 @@ import org.apache.maven.project.MavenProject;
 import org.bson.Document;
 import org.pavelreich.saaremaa.mongo.MongoDBClient;
 
-import com.github.mauricioaniche.ck.CSVExporter;
+import com.github.mauricioaniche.ck.CKClassResult;
+import com.github.mauricioaniche.ck.plugin.CKMetricsMojo;
+import com.github.mauricioaniche.ck.plugin.MetricsCSVWriter;
+import com.github.mauricioaniche.ck.plugin.MetricsWriter;
+
 
 @Mojo(name = "metrics", defaultPhase = LifecyclePhase.PROCESS_SOURCES, requiresDependencyResolution = ResolutionScope.NONE)
 public class CodeMetricsMojo extends AbstractMojo {
@@ -30,66 +34,97 @@ public class CodeMetricsMojo extends AbstractMojo {
 	@Parameter(defaultValue = "${project}", readonly = true, required = true)
 	MavenProject project;
 	private MongoDBClient db;
-	private static final String delim = ";";
+	private InternalMojo mojo;
 
 	public CodeMetricsMojo() {
+		super();
 		db = new MongoDBClient(getClass().getSimpleName());
-	}
-	public void execute() throws MojoExecutionException {
-		processDir(project.getCompileSourceRoots());
-		processDir(project.getTestCompileSourceRoots());
-		db.waitForOperationsToFinish();
+		mojo = new InternalMojo();
 	}
 
-	private void processDir(List<String> dirs) {
-		getLog().info("dirs: " + dirs);
-		dirs.stream().forEach(dirName -> {
+
+
+	class InternalMojo extends CKMetricsMojo {
+		@Override
+		protected MetricsWriter createMetricsWriter(String dirName) {
+			MetricsWriter csvWriter = super.createMetricsWriter(dirName);
+			if (csvWriter instanceof MetricsCSVWriter) {
+				return new MongoMetricsWriter(dirName, (MetricsCSVWriter) csvWriter);
+			} else {
+				return csvWriter;
+			}
+		}
+		
+		@Override
+		public void execute() throws MojoExecutionException {
+			this.project = CodeMetricsMojo.this.project;
+			super.execute();
+		}
+		
+	}
+	
+	class MongoMetricsWriter implements MetricsWriter {
+
+		private MetricsCSVWriter csvWriter;
+		private String dirName;
+
+		public MongoMetricsWriter(String dirName, MetricsCSVWriter csvWriter) {
+			this.csvWriter = csvWriter;
+			this.dirName = dirName;
+		}
+
+		@Override
+		public void notify(CKClassResult result) {
+			csvWriter.notify(result);
+		}
+
+		@Override
+		public void finish() {
+			csvWriter.finish();
+			exportToMongo(dirName, "class", csvWriter.getClassFileName());
+			exportToMongo(dirName, "method", csvWriter.getMethodFileName());
+			exportToMongo(dirName, "variable", csvWriter.getVariableFileName());
+			exportToMongo(dirName, "field", csvWriter.getFieldFileName());
+			db.waitForOperationsToFinish();
+		}
+
+		private void exportToMongo(String dirName, String docName, String fname) {
+			CSVParser parser;
 			try {
-				getLog().info("Processing " + dirName);
-				if (new File(dirName).exists()) {
-					String classFname = resolveFileName(dirName, CSVExporter.classFileName);
-					String methodFname = resolveFileName(dirName, CSVExporter.methodFileName);
-					String varFname = resolveFileName(dirName, CSVExporter.variableFileName);
-					String fieldFname = resolveFileName(dirName, CSVExporter.fieldFileName);
+				parser = CSVParser.parse(new File(fname), Charset.defaultCharset(),
+						CSVFormat.DEFAULT.withFirstRecordAsHeader());
+				Map<String, Integer> headerMap = parser.getHeaderMap();
+				getLog().info("dirName: " + dirName + ", fname: " + fname + ", headers: " + headerMap);
+				if (headerMap != null) {
+					List<Document> documents = parser.getRecords().stream()
+							.map(record -> toDocument(dirName, headerMap.keySet(), record)).collect(Collectors.toList());
+					getLog().info("From " + fname + " read " + documents.size() + " documents");
+					if (!documents.isEmpty()) {
+						db.insertCollection(docName + "Metrics", documents);
+					}
 
-					CSVExporter.processDirectory(dirName, delim, classFname, methodFname, varFname, fieldFname);
-					exportToMongo(dirName, "class", classFname);
-					exportToMongo(dirName, "method", methodFname);
-					exportToMongo(dirName, "variable", varFname);
-					exportToMongo(dirName, "field", fieldFname);
 				}
-			} catch (Exception e) {
-				getLog().error(e.getMessage(), e);
+			} catch (IOException e) {
+				throw new RuntimeException(e.getMessage(), e);
 			}
-		});
-	}
-	
-	private void exportToMongo(String dirName, String docName, String fname) throws IOException {
-        CSVParser parser = CSVParser.parse(new File(fname), Charset.defaultCharset(),
-                CSVFormat.newFormat(';').withFirstRecordAsHeader());
 
-		Map<String, Integer> headerMap = parser.getHeaderMap();
-		getLog().info("dirName: " + dirName + ", fname: " + fname +", headers: " + headerMap);
-		if (headerMap != null) {
-			List<Document> documents = parser.getRecords().stream().map(record -> toDocument(dirName, headerMap.keySet(), record)).collect(Collectors.toList());
-			getLog().info("From " + fname + " read " + documents.size() + " documents");
-			if (!documents.isEmpty()) {
-				db.insertCollection(docName + "Metrics", documents);				
+		}
+		
+
+		private Document toDocument(String dirName, Set<String> cols, CSVRecord record) {
+			Document doc = new Document().append("project", project.getArtifact().getId()).append("dirName", dirName);
+			for (String col : cols) {
+				doc = doc.append(col, record.get(col));
 			}
-			
+			return doc;
 		}
+
 	}
 
-	private Document toDocument(String dirName, Set<String> cols, CSVRecord record) {
-		Document doc = new Document().append("project", project.getArtifact().getId()).append("dirName", dirName);
-		for (String col : cols) { 
-			doc = doc.append(col, record.get(col));
-		}
-		return doc;
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		mojo.execute();
 	}
 
-	private String resolveFileName(String dirName, String fname) {
-		return Paths.get(dirName, fname).toString();
-	}
-	
+
 }
