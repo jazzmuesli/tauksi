e2e8a5700ba62000e88919b818808a5db72fd9cd
Pavel Reich <preich@mhetzner.openfun.org>
2019-10-16 03:14:26 +0200

    bind to localhost

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,7 @@ docker stop $container_id
 docker rm $container_id
 docker build . -t $id:v2
 # non-interactive
-docker run -p 37017:27017 -it --name $container_id $id:v2 /bin/bash
+docker run -p 127.0.0.1:37017:27017 -it --name $container_id $id:v2 /bin/bash
 #docker cp tauksi-container:/root/metrics.zip ./
 
 # attach if necessary: docker  exec -it tauksi-container /bin/bash
