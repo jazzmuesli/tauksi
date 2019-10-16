f436f65f5a1e55bbc54cfd3f80352420850f3459
preich <preich@192.168.1.101>
2019-10-16 01:10:46 +0100

    flip versions

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -6,7 +6,8 @@ do
 	cd /projects && git clone $i
 done
 
-#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# update-alternatives --config javac
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 java -version 2>&1 | tee /logs/java.version
 javac -version 2>&1 | tee /logs/javac.version
 
@@ -17,10 +18,12 @@ do
 	git clean -f -d	
 	root="/logs/`pwd | tr '/' '_'`"
 	mkdir -p $root
+	export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 	mvn compile | tee $root/compile.txt
 	mvn org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent install | tee $root/inst.txt
 	mvn com.google.testability-explorer:maven-testability-plugin:testability | tee $root/testability.txt
-	mvn -DmemoryInMB=9000 -Dcores=6 -DtimeInMinutesPerClass=2 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate org.evosuite.plugins:evosuite-maven-plugin:LATEST:export | tee $root/evosuite.txt
+	export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ && mvn -DmemoryInMB=9000 -Dcores=6 -DtimeInMinutesPerClass=2 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate org.evosuite.plugins:evosuite-maven-plugin:LATEST:export | tee $root/evosuite.txt
+	export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 	mvn org.pavelreich.saaremaa:plugin:metrics | tee $root/metr.txt
 	mvn -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=15 org.pavelreich.saaremaa:plugin:ctest | tee $root/ctest.txt
 	mvn -Dverbose=true -DwithHistory -DoutputFormats=CSV,XML,HTML  org.pitest:pitest-maven:mutationCoverage | tee $root/pitest.txt
