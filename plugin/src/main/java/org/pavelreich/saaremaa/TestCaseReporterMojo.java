e18bc9f31cb5caeb377d3881944f0246e6fd4f4b
preich <preich@192.168.1.101>
2019-11-14 23:13:30 +0000

    count testcases

---
+++ plugin/src/main/java/org/pavelreich/saaremaa/TestCaseReporterMojo.java
@@ -0,0 +1,55 @@
+package org.pavelreich.saaremaa;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.slf4j.Logger;
+
+@Mojo(name = "analyse-testcases", defaultPhase = LifecyclePhase.PROCESS_SOURCES, requiresDependencyResolution = ResolutionScope.NONE)
+public class TestCaseReporterMojo extends AbstractMojo {
+
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	MavenProject project;
+
+	public TestCaseReporterMojo() {
+		super();
+	}
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		try {
+			Logger logger = new MavenLoggerAsSLF4jLoggerAdaptor(getLog());
+			CombinedTestExtractor extractor = new CombinedTestExtractor(new SurefireTestExtractor(logger),
+					new SpoonTestExtractor(logger));
+			for (String dirName : project.getTestCompileSourceRoots()) {
+				getLog().info("Processing dir=" + dirName);
+
+				Map<String, Set<String>> testCasesMap = extractor.extractTestCasesByClass(dirName);
+				CSVReporter reporter = new CSVReporter(dirName + File.separator + "testcases.csv", "testClassName",
+						"testCaseName", "i");
+				for (Entry<String, Set<String>> entry : testCasesMap.entrySet()) {
+					int i = 0;
+					for (String testCase : entry.getValue()) {
+						reporter.write(entry.getKey(), testCase, i++);
+					}
+					reporter.write(entry.getKey(), "TOTAL", i);
+				}
+				reporter.close();
+			}
+		} catch (Exception e) {
+			getLog().error(e.getMessage(), e);
+		}
+
+	}
+
+}
