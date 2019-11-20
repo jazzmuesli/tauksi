919c6d11a893871d35c560961dd3aaa2fdba080e
preich <preich@192.168.1.101>
2019-11-20 01:25:34 +0000

    ignore evosuite for now

--- docker/run_metrics.sh
+++ docker/run_metrics.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/; export PATH=$JAVA_HOME/bin:$PATH
 mvn compile
-mvn -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
-mvn compile
+#mvn -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
+#mvn compile
 mvn org.pavelreich.saaremaa:plugin:metrics
-mvn -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
-mvn org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
-mvn -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
+#mvn -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
+#mvn org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
+#mvn -Drat.skip=true  -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
+mvn -Drat.skip=true -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=65 org.pavelreich.saaremaa:plugin:ctest
+#mvn -Dsandbox_mode=OFF
 mvn org.pavelreich.saaremaa:plugin:analyse-testcases
 mvn org.pavelreich.saaremaa:plugin:combine-metrics
 
