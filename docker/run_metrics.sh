45b7c56a979e10f48764d0a2bd47786f264420da
preich <preich@192.168.1.101>
2019-11-27 01:24:38 +0000

    sandbox off

--- docker/run_metrics.sh
+++ docker/run_metrics.sh
@@ -4,9 +4,10 @@ mvn compile
 #mvn -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
 #mvn compile
 mvn org.pavelreich.saaremaa:plugin:metrics
-#mvn -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
-#mvn org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
+#mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
+#mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
 #mvn -Drat.skip=true  -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
+mvn org.pavelreich.saaremaa:plugin:metrics
 mvn -Drat.skip=true -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=65 org.pavelreich.saaremaa:plugin:ctest
 #mvn -Dsandbox_mode=OFF
 mvn org.pavelreich.saaremaa:plugin:analyse-testcases
