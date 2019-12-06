be518e2ab2ceb0f9625c1920e8b04277d18738cf
preich <preich@192.168.1.101>
2019-12-06 23:07:58 +0000

    evosuite

--- docker/run_metrics.sh
+++ docker/run_metrics.sh
@@ -3,10 +3,17 @@
 echo "params:$*"
 ignoreChildProjects=true
 run_tests=true
+evosuite=false
 if [[ $* == *"skip_ctest"* ]]; then
   echo "skip ctest"
   run_tests=false
 fi
+
+if [[ $* == *"evo"* ]]; then
+  echo "run evosuite"
+  evosuite=true
+fi
+
 if [[ $* == *"ignore_child_projects"* ]]; then
   echo "ignore child projects"
   ignoreChildProjects=false
@@ -17,10 +24,15 @@ mvn -Drat.skip=true test-compile
 #mvn -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
 #mvn compile
 mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
-#mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
-#mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
+
+if [[ "$evosuite" == "true" ]];
+then
+	mvn -Drat.skip=true -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
+	mvn -Drat.skip=true -Dsandbox_mode=OFF -Duse_separate_classloader=false -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
+	mvn -Drat.skip=true -Dsandbox_mode=OFF -Duse_separate_classloader=false org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
+	mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
+fi
 #mvn -Drat.skip=true  -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
-mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
 mvn -Drat.skip=true com.google.testability-explorer:testability-mvn-plugin:testability
 mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:parse-testability
 if [[ "$run_tests" == "true" ]];
