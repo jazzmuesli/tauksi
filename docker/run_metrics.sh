aac1143cafacb88386a74a497f48008c4e1c76e8
preich <preich@192.168.1.101>
2019-12-06 21:07:54 +0000

    parse testability

--- docker/run_metrics.sh
+++ docker/run_metrics.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/; export PATH=$JAVA_HOME/bin:$PATH
-mvn test-compile
+mvn -Drat.skip=true test-compile
 #mvn -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
 #mvn compile
-mvn org.pavelreich.saaremaa:plugin:metrics
+mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
 #mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
 #mvn -Dsandbox_mode=OFF -Duse_separate_classloader=false org.evosuite.plugins:evosuite-maven-plugin:LATEST:export
 #mvn -Drat.skip=true  -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
-mvn org.pavelreich.saaremaa:plugin:metrics
+mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
+mvn -Drat.skip=true com.google.testability-explorer:testability-mvn-plugin:testability
+mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:parse-testability
 mvn -Drat.skip=true -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=165 org.pavelreich.saaremaa:plugin:ctest
 #mvn -Dsandbox_mode=OFF
-mvn org.pavelreich.saaremaa:plugin:analyse-testcases
-mvn org.pavelreich.saaremaa:plugin:combine-metrics
+mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:analyse-testcases
+mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:combine-metrics
 
