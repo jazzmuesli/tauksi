5400513186e40e750775892b69a9cee9de200fb0
preich <preich@192.168.1.101>
2019-12-10 22:01:07 +0000

    use getops

--- docker/run_metrics.sh
+++ docker/run_metrics.sh
@@ -30,7 +30,7 @@ mvn -Drat.skip=true test-compile
 #mvn compile
 mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:metrics
 
-if [[ "$evosuite" == "true" ]];
+if [ "$evosuite" = "true" ];
 then
 	mvn -Drat.skip=true -Ddependencies="org.evosuite:evosuite-standalone-runtime:LATEST:test;junit:junit:4.12:test" -Doverwrite=true org.pavelreich.saaremaa:plugin:add-dependency
 	mvn -Drat.skip=true -Dsandbox_mode=OFF -Duse_separate_classloader=false -DmemoryInMB=4000 -Dcores=6 -DtimeInMinutesPerClass=1 org.evosuite.plugins:evosuite-maven-plugin:LATEST:generate
@@ -41,7 +41,7 @@ fi
 #mvn -Drat.skip=true  -Dmaven.compiler.source=1.8 -Dmaven.compiler.target=1.8 -fae -Dmaven.test.failure.ignore=true org.jacoco:jacoco-maven-plugin:LATEST:prepare-agent test
 mvn -Drat.skip=true com.google.testability-explorer:testability-mvn-plugin:testability
 mvn -Drat.skip=true org.pavelreich.saaremaa:plugin:parse-testability
-if [[ "$run_tests" == "true" ]];
+if [ "$run_tests" = "true" ];
 then
 	mvn -Drat.skip=true -DseqTestMethods=false -DshuffleTests=true -DinterceptorEnabled=false -Dtimeout=165 org.pavelreich.saaremaa:plugin:ctest
 else
