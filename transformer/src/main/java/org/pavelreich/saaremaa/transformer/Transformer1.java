a2b3853ba3f71189fc63bbb397ce529ad26ff85e
preich <preich@192.168.1.101>
2020-01-06 00:50:53 +0000

    more tests

--- transformer/src/main/java/org/pavelreich/saaremaa/transformer/Transformer1.java
+++ transformer/src/main/java/org/pavelreich/saaremaa/transformer/Transformer1.java
@@ -1,10 +1,21 @@
 package org.pavelreich.saaremaa.transformer;
 
-
 public class Transformer1 {
 
 	public int add(int i, int j) {
-		return i+j;
+		if (i < 0) {
+			return i + j;
+		}
+		if (i > 30) {
+			return i + j;
+		}
+		return i + j + 0;
 	}
 
+	public double divide(double a, double b) {
+		if (b != 0) {
+			return a / b;
+		}
+		return 0.0;
+	}
 }
--- transformer/src/test/java/org/pavelreich/saaremaa/transformer/Transformer1Test.java
+++ transformer/src/test/java/org/pavelreich/saaremaa/transformer/Transformer1Test.java
@@ -8,8 +8,15 @@ public class Transformer1Test {
 	Transformer1 transformer1 = new Transformer1();
 
 	@Test
-	public void test() {
-		int result = transformer1.add(3,4);
+	public void testAdd() {
+		int result = transformer1.add(3, 4);
 		assertEquals(7, result);
 	}
+
+	@Test
+	public void testDivide() {
+		double result = transformer1.divide(3, 4);
+		assertEquals(0.75, result, 1e-6);
+	}
+
 }
