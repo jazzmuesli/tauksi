591007173a0f375260f37300aeafdbdcd032f291
preich <preich@192.168.1.101>
2020-01-23 00:23:15 +0000

    add depDir

--- plugin/src/main/java/org/pavelreich/saaremaa/AddDependencyMojo.java
+++ plugin/src/main/java/org/pavelreich/saaremaa/AddDependencyMojo.java
@@ -2,9 +2,19 @@ package org.pavelreich.saaremaa;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
+import java.net.URL;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,6 +26,7 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -23,6 +34,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriterSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -30,6 +43,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
 
 /**
@@ -48,26 +62,88 @@ public class AddDependencyMojo extends AbstractMojo {
 	private String dependencies;
 	@Parameter(property = "overwrite", defaultValue = "false")
 	private String overwrite;
+	@Parameter(property = "depFile", defaultValue = "")
+	private String depFile;
+	@Parameter(property = "depDir", defaultValue = "")
+	private String depDir;
 
 	public void execute() throws MojoExecutionException {
 		File file = project.getFile();
 		getLog().info("Reading from pom.xml: " + file);
+		MavenLoggerAsSLF4jLoggerAdaptor logger = new MavenLoggerAsSLF4jLoggerAdaptor(getLog());
 		try {
-			if (dependencies == null || dependencies.trim().isEmpty()) {
-				getLog().info(
-						"No new dependencies provided. Run with -Ddependencies=\"org.junit:junit:4.12;org.banana:core:3.14\"");
+			if (depDir != null && !depDir.trim().isEmpty()) {
+				Path path = new File(depDir).toPath();
+				List<String> files = java.nio.file.Files.walk(path).
+						filter(p -> p.toFile().getName().endsWith(".jar")).
+						map(f -> f.toFile().getAbsolutePath()).
+						collect(Collectors.toList());
+				logger.info("Found " + files + " dependencies");
+				Set<String> deps = new TreeSet<String>();
+				for (String f: files) {
+					deps.addAll(findDependency(f, logger));
+				}
+				dependencies = deps.stream().collect(Collectors.joining(";"));
+				logger.info("Will add dependencies " + dependencies);
+			} else if (depFile != null && !depFile.trim().isEmpty()) {
+				List<String> deps = findDependency(depFile, logger);
+				dependencies = deps.stream().collect(Collectors.joining(";"));
+			} else if (dependencies == null || dependencies.trim().isEmpty()) {
+				logger.info("No new dependencies provided. Run with -DdepDir=lib/ -Ddependencies=\"org.junit:junit:4.12;org.banana:core:3.14\"");
 				return;
 			}
-			addDependency(file, Boolean.valueOf(overwrite), dependencies, new MavenLoggerAsSLF4jLoggerAdaptor(getLog()));
+			addDependency(file, Boolean.valueOf(overwrite), dependencies, logger);
 		} catch (Exception e) {
 			getLog().error("Can't parse  " + file + " due to " + e.getMessage(), e);
 		}
 
 	}
 
+	/**
+	 * example
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 		Logger logger = LoggerFactory.getLogger(AddDependencyMojo.class);
+		findDependency("lib/ehcache-1.2.3.jar", logger);
 		addDependency(new File("pom.xml"), false, "apple:banana:1.0:test;apple:orange:1.3", logger);
+	}
+
+	private static List<String> findDependency(String fname, Logger logger) throws IOException {
+		byte[] bytes = java.nio.file.Files.readAllBytes(new File(fname).toPath());
+		String sha1code = Hashing.sha1().hashBytes(bytes).toString();
+		List<String> deps = new ArrayList<>();
+		String url = "http://search.maven.org/solrsearch/select?q=1:%22" + sha1code + "%22&rows=20&wt=json";
+		logger.info("For file " + fname + " got SHA1: " + sha1code + ", requesting " + url);
+		InputStream in = new URL(url).openStream();
+		try {
+			String json = IOUtils.toString(in);
+			JsonWriterSettings writerSettings = new JsonWriterSettings(JsonMode.SHELL, true);
+			org.bson.Document parsed = org.bson.Document.parse(json.toString());
+			if (parsed != null && parsed.containsKey("response")) {
+				org.bson.Document response = parsed.get("response", org.bson.Document.class);
+				int found = response.getInteger("numFound", 0);
+				if (found > 0) {
+					List<org.bson.Document> docs = response.getList("docs", org.bson.Document.class);
+					for (org.bson.Document doc : docs) {
+						String dep = doc.getString("id");
+						if (deps.isEmpty()) {
+							deps.add(dep);							
+						} else {
+							logger.info("Already have dependency " + deps + ", ignoring " + dep);
+						}
+					}
+				}
+				logger.info("For file " + fname + " found " + found + " dependencies: " + deps);
+			} else {
+				logger.info("Parsed result: " + parsed.toJson(writerSettings));
+			}
+
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		return deps;
 	}
 
 	private static void addDependency(File srcFile, boolean overwrite, String newDependencies, Logger logger) throws Exception {
