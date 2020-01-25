8689ec1da2423f42700fd188f087850f912ec0aa
preich <preich@192.168.1.101>
2020-01-25 09:41:52 +0000

    optimise

--- sf110/metrics.sh
+++ sf110/metrics.sh
@@ -11,18 +11,35 @@ work() {
 }
 for i in `ls /sf110 | grep ^[0-9]`;
 do
-	if [ -f /sf110/$i/target/testability.csv ];
-        then
-                echo "testan already present $i"
-        else
-		# mvn -Doverwrite=true -DdepDir=lib/  org.pavelreich.saaremaa:plugin:add-dependency
-                sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:testan com.google.testability-explorer:testability-mvn-plugin:testability org.pavelreich.saaremaa:plugin:parse-testability"
-        fi
-	if [ -f /sf110/$i/target/metrics1.csv ];
+	if [ -f /sf110/$i/target/ck.ok ];
 	then
-		echo "already present $i"
+		echo "ck already present $i"
+    else
+                sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:metrics && touch /sf110/$i/target/ck.ok"
+    fi
+	if [ -f /sf110/$i/target/testan.ok ];
+	then
+		echo "testan already present $i"
+    else
+                sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:testan && touch /sf110/$i/target/testan.ok"
+    fi
+	if [ -f /sf110/$i/target/testcases.ok ];
+	then
+		echo "ck already present $i"
+    else
+                sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:analyse-testcases && touch /sf110/$i/target/testcases.ok"
+    fi
+	if [ -f /sf110/$i/target/testability.xml ];
+	then
+		echo "testability already present $i"
+    else
+                sem -j+0 "cd /sf110/$i && mvn -o com.google.testability-explorer:testability-mvn-plugin:testability org.pavelreich.saaremaa:plugin:parse-testability"
+    fi
+	if [ -f /sf110/$i/target/metrics.csv ];
+	then
+		echo "metrics already present $i"
 	else
-		sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:metrics org.pavelreich.saaremaa:plugin:analyse-testcases org.pavelreich.saaremaa:plugin:combine-metrics"
+		sem -j+0 "cd /sf110/$i && mvn -o org.pavelreich.saaremaa:plugin:combine-metrics"
 	fi
 done
 sem --wait
