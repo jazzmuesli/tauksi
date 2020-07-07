7da3031ec933018e07b923ff3a1b656ab7a87b36
preich <preich@192.168.1.101>
2020-07-08 00:06:12 +0200

    clone more projects and bind

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-id=tauksi3
+id=tauksi4
 container_id="$id-container"
 docker stop $container_id
 docker rm $container_id
 docker build . -t $id:v2
 # non-interactive
-docker run -p 127.0.0.1:37017:27017 -it --name $container_id $id:v2 /bin/bash
+PROJ_DIR=$HOME/projects
+ROOT_DIR=$HOME/$id
+docker run -v $PROJ_DIR:/projects -v $ROOT_DIR:/root -p 127.0.0.1:37017:27017 -it --name $container_id $id:v2 /bin/bash
 #docker cp tauksi-container:/root/metrics.zip ./
 
 # attach if necessary: docker  exec -it tauksi-container /bin/bash
