4641c2d7ad5a0567ed48d210b5285ab503470534
preich <preich@192.168.1.101>
2020-07-14 20:58:00 +0200

    combine files

---
+++ docker/combine.r
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+#/Library/Frameworks/R.framework/Versions/3.5/Resources/bin/Rscript 
+
+
+print("hellooo")
+args=commandArgs(trailing=T)
+out_fname=ifelse(length(args)==1, args[1], "combined-metrics.csv")
+library(plyr)
+library(data.table)
+input<-file('stdin', 'r')
+files <- readLines(input)
+data=data.frame()
+for (f in files) {
+	x=data.table::fread(file=f, sep=";")
+	data=rbind.fill(data, x)
+}
+print(out_fname)
+write.table(data, out_fname, row.names=F, sep=";")
