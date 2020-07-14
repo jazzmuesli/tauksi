18d8dea1a2f6426dbd0c921776bdcfae800b074c
preich <preich@192.168.1.101>
2020-07-14 21:36:34 +0200

    fix

--- docker/combine.r
+++ docker/combine.r
@@ -2,7 +2,6 @@
 #/Library/Frameworks/R.framework/Versions/3.5/Resources/bin/Rscript 
 
 
-print("hellooo")
 args=commandArgs(trailing=T)
 out_fname=ifelse(length(args)==1, args[1], "combined-metrics.csv")
 library(plyr)
@@ -12,6 +11,7 @@ files <- readLines(input)
 data=data.frame()
 for (f in files) {
 	x=data.table::fread(file=f, sep=";")
+	x$filename=f
 	data=rbind.fill(data, x)
 }
 print(out_fname)
